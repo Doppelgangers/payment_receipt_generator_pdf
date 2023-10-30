9501061230a4d337334c26277a5ac9241eaf7a6a
Doppelgangers <6666genocide666@mail.ru>
2023-10-30 15:22:14 +0300

    Рефакторинг класса PaymentReceiptData

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ class PaymentReceiptData:
             return self._date
 
         @property
-        def str_month_and_year(self) -> str:
+        def month_and_short_year(self) -> str:
             """
             Выводит дату в формете короткого года+месяц
             date(13.05.2023) -> 2305
@@ -65,6 +65,15 @@ class PaymentReceiptData:
             if len(api_date) == 6:
                 api_date = api_date[2:]
             return api_date
+
+        @property
+        def month_word_and_year(self) -> str:
+            """
+            date(13.05.2023) -> Май 2023 г.
+            """
+            months = ['Январь', 'февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+                      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+            return f"{months[int(self.date_obj.month) - 1]} {int(self.date_obj.year)} г."
 
         @property
         def payment_period(self):
@@ -81,25 +90,26 @@ class PaymentReceiptData:
             return f"{self.__class__.__name__}({self.__str__()})"
 
     def __init__(self, json_data):
-        self.organization = ''
-        self.department = ''
-        self.inn = ''
-        self.kpp = ''
-        self.personal_account = ''
-        self.current_account = ''
-        self.bank_name = ''
-        self.bik = ''
-        self.correspondent_account = ''
-        self.full_name = ''
-        self.client_personal_account = ''
-        self.agreement_date = ''
-        self.kbk = ''
-        self.purpose_of_payment = ''
-        self.date_payment = ''
-        self.kind_of_activity = ''
-        self.total_sum = 0.0
-        self.kindergarten_group = ''
+        self.organization = None
+        self.department = None
+        self.inn = None
+        self.kpp = None
+        self.personal_account = None
+        self.current_account = None
+        self.bank_name = None
+        self.bik = None
+        self.correspondent_account = None
+        self.full_name = None
+        self.client_personal_account = None
+        self.agreement_date: PaymentReceiptData.PDate = None
+        self.kbk = None
+        self.purpose_of_payment = None
+        self.date_payment: PaymentReceiptData.PDate = None
+        self.kind_of_activity = None
+        self.total_sum: PaymentReceiptData.Money = None
+        self.kindergarten_group = None
 
+        # Инициализация атрибутов классада
         self.serialize_json(json_data)
 
     @property
@@ -132,22 +142,46 @@ class PaymentReceiptData:
         self.date_payment = self.PDate(self.date_payment)
         self.agreement_date = self.PDate(self.agreement_date)
 
+    @property
+    def context_item(self) -> dict:
+        context_item = {
+            "organization": self.organization,
+            "department": self.department,
+            "inn": self.inn,
+            "kpp": self.kpp,
+            "personal_account": self.personal_account,
+            "current_account": self.personal_account,
+            "bank_name": self.bank_name,
+            "bik": self.bik,
+            "correspondent_account": self.correspondent_account,
+            "full_name": self.full_name,
+            "client_personal_account": self.client_personal_account,
+            "agreement_date": self.agreement_date,
+            "kbk": self.kbk,
+            "purpose_of_payment": self.purpose_of_payment,
+            "date_payment": self.date_payment.month_word_and_year,
+            "kind_of_activity": self.kind_of_activity,
+            "total_sum": self.total_sum.__str__(),
+            "kindergarten_group": self.kindergarten_group
+        }
+
+
+
+        return context_item
+
 
 class Codification:
 
-    class CodificationCode:
+    class QRCode:
 
-        def generate(self):
-            pass
+        def __init__(self):
+            self.qr_data: list[dict] = None
 
-        def get_codification_str(self):
-            pass
-
-    class QRCode(CodificationCode):
-        def generate(self) -> BytesIO:
+        def generate(self, qr_data: str = None) -> BytesIO:
+            qr_data = qr_data if qr_data else self.qr_data
             # Создаем изображение QR-кода
             qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=32, border=2)
-            qr.add_data(self.get_codification_str())
+            qr.add_data(self.get_codification_str(qr_data))
             qr.make(fit=True)
             qr_image = qr.make_image(fill_color="black", back_color="white")
             # Создаем объект BytesIO и сохраняем изображение QR-кода в него
@@ -156,35 +190,38 @@ class Codification:
             qr_bytes.seek(0)
             return qr_bytes
 
-        def get_codification_str(self, attribute_list: [dict]):
+        def get_codification_str(self, attribute_list: [dict] = None):
+            attribute_list = attribute_list if attribute_list else self.qr_data
             qr_data = 'ST00012|'
             for arg in attribute_list:
                 qr_data += f'{arg["name"]}={arg["value"]}|'
             return qr_data
 
-        def get_attribute_list_in_payment_data(self, payment_data: PaymentReceiptData):
-            data = []
+        def get_attribute_list_in_payment_data(self, payment_data: PaymentReceiptData) -> list[dict]:
+            qr_data = \
+                [
+                    {"name": "Name", "value": payment_data.name},
+                    {"name": "PersonalAcc", "value": payment_data.current_account},
+                    {"name": "BankName", "value": payment_data.bank_name},
+                    {"name": "BIC", "value": payment_data.bik},
+                    {"name": "CorrespAcc", "value": payment_data.correspondent_account},
+                    {"name": "PayeeINN", "value": payment_data.inn},
+                    {"name": "PersonalAccount", "value": payment_data.personal_account},
+                    {"name": "PersAcc", "value": payment_data.client_personal_account},
+                    {"name": "Category", "value": payment_data.kind_of_activity},
+                    {"name": "PaymPeriod", "value": payment_data.date_payment.payment_period},
+                    {"name": "Sum", "value": payment_data.total_sum.api_format}
+                ]
+            self.qr_data = qr_data
+            return qr_data
 
-            {"name": "Name", "value": payment_data.name}
-            {"name": "PersonalAcc", "value": payment_data.current_account}
-            {"name": "BankName", "value": payment_data.bank_name}
-            {"name": "BIC", "value": payment_data.bik}
-            {"name": "CorrespAcc", "value": payment_data.correspondent_account}
-            {"name": "PayeeINN", "value": payment_data.inn}
-            {"name": "PersonalAccount", "value": payment_data.org_pres_acc}
-            {"name": "PersAcc", "value": payment_data.client_pers_acc}
-            {"name": "Category", "value": payment_data.kind_of_activity}
-            {"name": "PaymPeriod", "value": }
-            {"name": "Sumtemplate", "value": }
-        template = f"ST0|Payee|PaymPeriod=20230500|Sum={summa}|"
+    class BarCode:
 
-    class BarCode(CodificationCode):
-
-        def __init__(self, org_pres_acc, client_pers_acc, payment_period, summa, kind_of_activity):
+        def __init__(self, org_pres_acc, client_pers_acc, payment_period: PaymentReceiptData.PDate, summa: PaymentReceiptData.Money , kind_of_activity):
             self.org_pres_acc = org_pres_acc
             self.client_pers_acc = client_pers_acc
-            self.payment_period: PaymentReceiptData.PDate = payment_period
-            self.summa: PaymentReceiptData.Money = summa
+            self.payment_period = payment_period
+            self.summa= summa
             self.kind_of_activity = kind_of_activity
 
         def generate(self) -> BytesIO:
@@ -196,83 +233,50 @@ class Codification:
             return barcode_bytes
 
         def get_codification_str(self) -> str:
-            return f"00000{self.org_pres_acc}{self.client_pers_acc}{self.payment_period.str_month_and_year}0000{self.summa.api_format}{self.kind_of_activity}"
+            return f"00000{self.org_pres_acc}{self.client_pers_acc}{self.payment_period.month_and_short_year}0000{self.summa.api_format}{self.kind_of_activity}"
+
+        def __str__(self):
+            return self.get_codification_str()
+
 
 class PaymentReceipt:
 
     def __init__(self, path_template: str | Path):
         self.path_template = path_template
-        self.data_payment = {}
 
-
-    def fill_docx_template(self, context, save_path_temp: str | Path = None) -> None:
-        if save_path_temp is None: save_path_temp = "temp.docx"
+    def fill_docx_template(self, list_json: list[str], save_path: str | Path = None) -> None:
+        if save_path is None: save_path = "temp.docx"
 
         # Инициализация шаблона
         template = DocxTemplate(self.path_template)
+        items = []
+        for json_data in list_json:
+            data_payment = PaymentReceiptData(json_data)
 
-        for data in context["items"]:
-            qr_data = self.generate_data_qrcode(name=f"{data['department']}({data['organization']})",
-                                                current_account=data["current_account"],
-                                                bank_name=data["bank_name"],
-                                                bik=data["bik"],
-                                                corresp_acc=data["correspondent_account"],
-                                                inn=data["inn"],
-                                                org_pres_acc=data["personal_account"],
-                                                client_pers_acc=data["client_personal_account"],
-                                                kind_of_activity=data["kind_of_activity"],
-                                                summa=data["total_sum"]
-                                                )
-            qr_file = self.generate_qrcode(qr_data)
+            qr_code = Codification.QRCode()
+            qr_code.get_attribute_list_in_payment_data(data_payment)
+            qr_file = qr_code.generate()
 
-            barcode_data = self.generate_data_barcode(
-                org_pres_acc=data["personal_account"],
-                client_pers_acc=data["client_personal_account"],
-                date_payment=self._serialize_date(data["date_payment"]),
-                summa=data["total_sum"],
-                kind_of_activity=data["kind_of_activity"]
+            bar_code = Codification.BarCode(
+                org_pres_acc=data_payment.personal_account,
+                client_pers_acc=data_payment.client_personal_account,
+                payment_period=data_payment.date_payment,
+                summa=data_payment.total_sum,
+                kind_of_activity=data_payment.kind_of_activity,
             )
-            barcode_file = self.generate_barcode(barcode_data)
+            barcode_file = bar_code.generate()
 
-            months = ['Январь', 'февраль', 'Март', 'Апрель', 'Май', 'Июнь',
-                      'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-            date_payment = self._serialize_date(data["date_payment"])
+            context_item: dict = data_payment.context_item
 
-            data["date_payment"] = f"{months[int(date_payment.month) - 1]} {int(date_payment.year)} г."
-            data["barcode_image"] = InlineImage(template, barcode_file, height=Mm(15))
-            data["qrcode_image"] = InlineImage(template, qr_file, width=Mm(28), height=Mm(28))
-            data["total_sum"] = f"{int(data['total_sum'])} руб. {int(round(data['total_sum'] % 1, 2) * 100)} коп."
+            context_item["barcode_image"] = InlineImage(template, barcode_file, height=Mm(15))
+            context_item["qrcode_image"] = InlineImage(template, qr_file, width=Mm(28), height=Mm(28))
+            items.append(context_item)
 
+        context = {"items": items}
         template.render(context)
-        # Сохранение pdf на основе шаблона
-        template.save(save_path_temp)
-        self.del_first_line_in_docx(save_path_temp)
-
-    def generate_data_barcode(self, org_pres_acc: str, client_pers_acc: str, date_payment: datetime.date, summa: float,
-                              kind_of_activity: str):
-        month = str(date_payment.month)
-        if len(month) == 1: month = "0" + month
-        year = str(date_payment.year)
-        year = year[2:] if (len(year) == 4) else year
-        summa = f"{int(summa)}{int(round(summa % 1, 2) * 100)}"
-        template = f"00000{org_pres_acc}{client_pers_acc}{month}{year}0000{summa}{kind_of_activity}"
-        return template
-
-    @staticmethod
-    def generate_data_qrcode(**kwargs):
-        name: str = kwargs.get("name")
-        current_account: str = kwargs.get("current_account")
-        bank_name: str = kwargs.get("bank_name")
-        bik: str = kwargs.get("bik")
-        corresp_acc: str = kwargs.get("corresp_acc")
-        inn: str = kwargs.get("inn")
-        org_pres_acc: str = kwargs.get("org_pres_acc")
-        client_pers_acc: str = kwargs.get("client_pers_acc")
-        kind_of_activity: str = kwargs.get("kind_of_activity")
-        summa: float = kwargs.get("summa")
-        summa = f"{int(summa)}{int(round(summa % 1, 2) * 100)}"
-        template = f"ST00012|Name={name}|PersonalAcc={current_account}|BankName={bank_name}|BIC={bik}|CorrespAcc={corresp_acc}|PayeeINN={inn}|PersonalAccount={org_pres_acc}|PersAcc={client_pers_acc}|Category={kind_of_activity}|PaymPeriod=20230500|Sum={summa}|"
-        return template
+        # Сохранение заполненного docx документа
+        template.save(save_path)
+        self.del_first_line_in_docx(save_path)
 
     @staticmethod
     def del_first_line_in_docx(path_docx: str | Path):
@@ -283,59 +287,13 @@ class PaymentReceipt:
         docx.save(path_docx)
 
 
-items = [
-    {
-        "organization": 'МАДОУ "Детский сад № 100"',
-        "department": "Департамент финансов г.Н.Новгорода",
-        "inn": "5260040678",
-        "kpp": "526001001",
-        "personal_account": "07040754581",
-        "current_account": "03234643227010003204",
-        "bank_name": "ВОЛГО-ВЯТСКОЕ ГУ БАНКА РОССИИ//УФК по Нижегородской области г. Нижний Новгород",
-        "bik": "012202102",
-        "correspondent_account": "40102810745370000024",
-        "full_name": "Тарасова Есения",
-        "client_personal_account": "4100100232",
-        "agreement_date": "01.10.20",
-        "kbk": "07507011130199404130",
-        "purpose_of_payment": "Оплата за Родительская плата за присмотр и уход за детьми.",
-        "date_payment": "01.05.2023",
-        "kind_of_activity": "04013",
-        "total_sum": 3193.20,
-        "kindergarten_group": "100 13 2 младшая"
-    },
-
-    {
-        "organization": 'МАДОУ "Детский сад № 100"',
-        "department": "Департамент финансов г.Н.Новгорода",
-        "inn": "5260040678",
-        "kpp": "526001001",
-        "personal_account": "07040754581",
-        "current_account": "03234643227010003204",
-        "bank_name": "ВОЛГО-ВЯТСКОЕ ГУ БАНКА РОССИИ//УФК по Нижегородской области г. Нижний Новгород",
-        "bik": "012202102",
-        "correspondent_account": "40102810745370000024",
-        "full_name": "Тарасова Есения",
-        "client_personal_account": "4100100323",
-        "agreement_date": "01.10.20",
-        "kbk": "07507011130199404130",
-        "purpose_of_payment": "Оплата за Родительская плата за присмотр и уход за детьми.",
-        "date_payment": "01.05.23",
-        "kind_of_activity": "04013",
-        "total_sum": 3193.20,
-        "kindergarten_group": "100 13 2 младшая"
-    },
-]
-
-data = {"items": items.copy()}
 
 if __name__ == '__main__':
     t = time.time()
-    receipts = PaymentReceipt("templates/test.docx")
 
     with open("data.json", "r", encoding="utf-8") as f:
         dat = f.read()
-    da = PaymentReceiptData(dat)
-    print(da.__dict__)
-    # receipts.fill_docx_template(data)
+
+    receipts = PaymentReceipt("templates/test.docx")
+    receipts.fill_docx_template([dat])
     print(time.time() - t)
